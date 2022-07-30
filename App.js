9c1c857ba3817a58c88514de0c53144ec8974c45
jimmy lelievre <jimmylelievre@iMac-de-jimmy.local>
2022-07-30 20:11:01 +0200

    First commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks aaa</Text>
+        <View style={styles.items}>
+          <Task />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: { fontSize: 24, fontWeight: "bold" },
+  items: {
+    marginTop: 30,
+  },
+});
